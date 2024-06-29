c3f0041e67d58cb60bd0c7542d6cbebf6d4ceb97
hxnn3sgit <seihannes@gmx.de>
2024-06-29 16:52:39 +0200

    overloaded operators for template list implemented

--- tt/09/list/list.cpp
+++ tt/09/list/list.cpp
@@ -18,28 +18,44 @@ private:
 public:
 	list() : head(nullptr) {};
 	list(const list &other); //c ctor
-	list(list &&other); //move ctor
+	//list(list &&other); //move ctor
 	~list();
 	int len() const;
 	void append(const T &to_append);
 	void prepend(const T &to_prepend);
 	void insert_at(const T &value, int index);
-	void delete_value(const T &value);
-	list &operator=(list other); //assigment
+	void delete_item(const T &value);
+	//list &operator=(list other); //assigment
 	template<typename U>
 	friend std::ostream& operator<<(std::ostream &out, const list<U> &my_list); 
 };
 
 class car {
 private:
-	int vMax;
 	string marke;
+	int vMax;
 public:
 	car() {}
 	car(int vMax, string marke) : vMax(vMax), marke(marke) {}
 	//destruktor wird nicht benötigt
+	bool operator==(const car &other);
+	bool operator!=(const car &other);
 	friend ostream &operator<<(ostream &out, const car &my_car);
 };
+
+bool car::operator==(const car &other) {
+	if (marke == other.marke && vMax == other.vMax)
+		return true;
+	else
+		return false;
+}
+
+bool car::operator!=(const car &other) {
+	if (marke != other.marke && vMax != other.vMax)
+		return true;
+	else
+		return false;
+}
 
 ostream &operator<<(ostream &out, const car &my_car) {
 	out << "marke: " << my_car.marke << ", vMax: " << my_car.vMax;
@@ -56,10 +72,10 @@ list<T>::list(const list &other) {
 	}
 }
 
-template<typename T>
+/*template<typename T>
 list<T>::list(list &&other) noexcept : head(other.head) {
 	other.head = nullptr;
-}
+}*/
 
 template<typename T>
 list<T>::~list() {
@@ -108,7 +124,7 @@ void list<T>::prepend(const T &to_prepend) {
 }
 
 template<typename T>
-void list<T>::delete_value(const T &value) {
+void list<T>::delete_item(const T &value) {
 	if(head->value == value) { //head must be deleted
 		node *temp = head;
 		head = head->next;
@@ -144,17 +160,7 @@ std::ostream& operator<<(std::ostream &out, const list<T> &my_list) {
 		return out << "list is empty";
 }
 
-int main(int argc, char **argv) {
-	list<int> my_int_list;
-	
-	if (argc > 1) {
-		for (int i = 1; i < argc; ++i)
-			my_int_list.prepend(atoi(argv[i]));
-
-		cout << my_int_list << endl;
-
-	}
-
+void do_things_with_cars() {
 	car car1(241, "VW");
 	car car2(400, "Bugatti");
 	car car3(220, "Opel");
@@ -165,9 +171,23 @@ int main(int argc, char **argv) {
 	my_list_of_cars.prepend(car1);
 	my_list_of_cars.prepend(car2);
 	my_list_of_cars.prepend(car3);
+	my_list_of_cars.prepend(car4);
 
 	cout << my_list_of_cars << endl;
+	my_list_of_cars.delete_item(car1);
+	cout << my_list_of_cars << endl;
+}
 
+void do_things_with_ints(int argc, char **argv) {
+	list<int> my_int_list;
+	if (argc > 1) {
+		for (int i = 1; i < argc; ++i)
+			my_int_list.prepend(atoi(argv[i]));
+		cout << my_int_list << endl;
+	}
+}
+
+void do_things_with_strings() {
 	string string1 = "hallo";
 	string string2 = "das";
 	string string3 = "ist";
@@ -184,10 +204,13 @@ int main(int argc, char **argv) {
 
 	cout << my_list_of_strings << endl;
 	
-	my_list_of_strings.delete_value("das");
+	my_list_of_strings.delete_item("das");
 
 	cout << my_list_of_strings << endl;
+}
 
-	my_list_of_cars.delete_value(car2);
-
+int main(int argc, char **argv) {
+	do_things_with_ints(argc, argv);
+	do_things_with_cars();
+	do_things_with_strings();
 }
