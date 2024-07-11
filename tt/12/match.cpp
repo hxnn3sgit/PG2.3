44a079f0cc274589fd9326fa18f0bb6efce8d18e
hxnn3sgit <seihannes@gmx.de>
2024-07-11 14:51:23 +0200

    updated

--- tt/12/match.cpp
+++ tt/12/match.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <regex>
+#include <string>
 
-using std::cout, std::endl;
+using std::cout, std::endl, std::string;
 
 int main(int argc, char **argv) {
+	if (argc != 2 ) {
+		cout << "Kein regex übergeben. Programm beenden..." << endl;
+		exit(-1);
+	}
+
+	std::string expr(argv[1]);
+	std::regex re {argv[1]};
+	string line;
 	
+	while(std::getline(std::cin, line)){
+		std::smatch m;
+		if (regex_match(line, m, re))
+			cout << line << endl;
+	}
+
 	return 0;
 }
